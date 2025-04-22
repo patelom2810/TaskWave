f4eae733a2768a88b9171a005ce5cb19f551bf23
OM PATEL <122160855+patelom2810@users.noreply.github.com>
2025-04-22 15:50:27 +0530

    Add files via upload

---
+++ js/firebase-config.js
@@ -0,0 +1,12 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyAOkalLzSTlGsH8j6G_vxz9zzX3GOq-eas",
+  authDomain: "col-todo.firebaseapp.com",
+  projectId: "col-todo",
+  storageBucket: "col-todo.firebasestorage.app",
+  messagingSenderId: "324403283385",
+  appId: "1:324403283385:web:dfe1bb0c8384efa28d6268",
+  measurementId: "G-9B5FM0YF9Y"
+};
+
+firebase.initializeApp(firebaseConfig);
